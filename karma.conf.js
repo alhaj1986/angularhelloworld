092d659602ab78bef8eedb4e4a147b10bb413f26
alhaj1986 <105738799+alhaj1986@users.noreply.github.com>
2025-04-15 15:15:19 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,60 +1,29 @@
 module.exports = function(config) {
   config.set({
-    // Base path that will be used to resolve all patterns (e.g., files, exclude)
     basePath: '',
-
-    // Frameworks to use
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-
-    // List of files / patterns to load in the browser
-    files: [],
-
-    // List of files / patterns to exclude
-    exclude: [],
-
-    // Preprocess matching files before serving them to the browser
-    preprocessors: {},
-
-    // Test results reporter to use
-    reporters: ['progress', 'kjhtml'],
-
-    // Web server port
-    port: 9876,
-
-    // Enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-    // Level of logging
-    logLevel: config.LOG_INFO,
-
-    // Enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-    // Start these browsers
-    browsers: ['ChromeHeadless'],
-
-    // Continuous Integration mode
-    singleRun: false,
-
-    // Concurrency level
-    concurrency: Infinity,
-
-    // Angular CLI configuration
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-
-    // Custom launcher for ChromeHeadless
-    customLaunchers: {
-      ChromeHeadless: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu']
-      }
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/your-project-name'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
     },
-
-    // Angular CLI configuration
-    angularCli: {
-      environment: 'dev'
-    }
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+   Watch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    restartOnFileChange: true
   });
 };
