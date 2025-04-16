ce529b4d7f8d7941ccc24a5e74b41fe13d0dc978
alhaj1986 <105738799+alhaj1986@users.noreply.github.com>
2025-04-16 12:26:25 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HelloService } from './services/hello.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  content?: string;
-
-  constructor(private helloService: HelloService) { }
-
-  ngOnInit(): void {
-    this.helloService.getServerMessage().subscribe({
-      next: data => {
-        this.content = data;
-      },
-      error: err => {
-        if (err.error) {
-          try {
-            const res = JSON.parse(err.error);
-            this.content = res.message;
-          } catch {
-            this.content = `Error with status: ${err.status} - ${err.statusText}`;
-          }
-        } else {
-          this.content = `Error with status: ${err.status}`;
-        }
-      }
-    });
-  }
+export class AppComponent {
+  title = 'Hello World';
 }
